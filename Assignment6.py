c0eb6561fa46094b68f5a48aa980916c977dc7c1
SkwarczynskiP <pskwarczynski2@oakland.edu>
2024-10-29 14:21:52 -0400

    Updated descriptions

--- Assignment6.py
+++ Assignment6.py
@@ -19,10 +19,10 @@ class Solution:
         return count
 
 # Time complexity: O(nlogn) where n is the maximum of the number of children and cookies.
-# The algorithm is correct because it sorts the children and cookies in ascending order and then iterates through the
-# children and cookies. If the cookie size is greater than or equal to the child's greed factor, the child is satisfied.
-# The algorithm returns the number of satisfied children.
-# You need to sort the arrays to make sure that the smallest cookie is given to the child with the smallest greed factor.
+# The algorithm sorts the children and cookies in ascending order and then iterates through the children and cookies.
+# If the cookie size is greater than or equal to the child's greed factor, the child is satisfied. The algorithm returns
+# the number of satisfied children. The algorithm is correct because it satisfies the children with the smallest greed
+# factor first. This is accomplished by sorting the arrays before iterating through the arrays.
 
 # Question 2
 # Give an algorithm to solve this problem. Give an argument as to why your algorithm is correct.
@@ -40,3 +40,8 @@ class Solution2:
                 num[i], num[j] = num[j], num[i]
                 break
         return int(''.join(num))
+
+# This algorithm sorts the digits of the number in descending order and then compares the digits of the original number
+# with the sorted digits. If the digits are different, the algorithm swaps the digits to get the maximum number.
+# The algorithm returns the maximum number after the swap. The algorithm is correct because it guarantees that the
+# maximum number is obtained by swapping the digits.
